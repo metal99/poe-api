ab0ce893db560a170d80568ac6e12adcbe7094dc
Jonathan Tew <metal99@gmail.com>
2024-12-24 20:42:04 -0700

    initial commit

---
+++ src/main/java/com/metal99/poeChallenge/PoeChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.metal99.poeChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoeChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoeChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/metal99/poeChallenge/PoeChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metal99.poeChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoeChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
